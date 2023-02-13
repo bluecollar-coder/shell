f50eb249d1ddad91d7dab3212797afd8ea106be6
bluecollar-coder <anishkumarmalik@gmail.com>
2023-02-13 18:51:38 -0500

    added topics

--- test.sh
+++ test.sh
@@ -3,3 +3,9 @@
 ls
 #list password file
 cat /etc/passwd
+
+# to provide current user executable permissions for a particular script e.g. for current test.sh script
+## chmod u+x test.sh
+## and by running above command you can execute/run the script by using './test.sh' and no need to provide 'bash test.sh'
+## also no need to add she-bang at top of script file
+## however it is best practise to write she-bang at top of file to make sure it is run by proper bash/way and understandable as well
