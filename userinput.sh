09152ac875d52fc32e7afdce3ea5561714cc038f
bluecollar-coder <anishkumarmalik@gmail.com>
2023-02-14 18:22:52 -0500

    Taking input from user

--- userinput.sh
+++ userinput.sh
@@ -3,4 +3,4 @@
 read -p "Enter your name:" name
 echo -e "Thanks for entering your name: \e[35m $name \e[0m"
 read -p "Enter to the course:" course
-echo "Welcome \e[32m$name\e[0m! Thanks for joining course: \e[34m $course \e[0m"
+echo -e "Welcome \e[32m$name\e[0m! Thanks for joining course: \e[34m $course \e[0m"
