8ace736a85d54155cea6b940e5dcb16256379f3c
bluecollar-coder <anishkumarmalik@gmail.com>
2023-02-13 20:06:44 -0500

    command substitution

--- variables.sh
+++ variables.sh
@@ -9,6 +9,12 @@ echo $s
 # also can be accessed with braces
 echo ${s}
 
-# for arithmetic operation use double bracket
+# for arithmetic operation use double brackets
 a=$((5*5))
 echo $a
+
+#command substitution use single brackets
+DATE=$(date +%F)
+echo$DATE
+DATE=$(date +%F-%H-%M-%S)
+echo$DATE
