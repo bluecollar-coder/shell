f290d79748ce8958f93f2ea723f82a8b952beadf
bluecollar-coder <anishkumarmalik@gmail.com>
2023-02-13 19:57:00 -0500

    comments

--- print.sh
+++ print.sh
@@ -10,6 +10,13 @@ echo Hello World
 echo -e "A\nB, C\tD, E\a\F"
 
 # print in colors - it's important to end that colors as well at the same time
+#color    codes   Syntax
+#RED      31      \e[31m
+#GREEN    32      \e[32m
+#YELLOW   33      \e[33m
+#BLUE     34      \e[34m
+#MAGENTA  35      \e[35m
+#CYAN     36      \e[36m
 
 echo -e "\e[31m HELLO in Red \e[0m"
 echo -e "\e[33m HELLO in Yellow"
