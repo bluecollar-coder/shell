c62debdaa11f9bda4789d0e3e7de8afd1d27479b
bluecollar-coder <anishkumarmalik@gmail.com>
2023-02-14 18:36:42 -0500

    input parameters variables

--- inputvariables.sh
+++ inputvariables.sh
@@ -8,6 +8,8 @@ echo -e "2md argument passed to the script \e[35m $0 \e[0m is:" $2
 
 echo -e "3rd argument passed to the script \e[35m $0 \e[0m is:" $3
 
+echo -e "10th argument passed to the script \e[35m $0 \e[0m is:" $(10)
+
 echo -e "All the arguments passed to the script \e[31m $0 \e[0m are:" $@
 
 echo -e "Total number of arguments passed to the script \e[32m $0 \e[0m are:" $#
