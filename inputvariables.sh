d7385f629fa45780a4548aab6fec185a81b5e886
bluecollar-coder <anishkumarmalik@gmail.com>
2023-02-14 18:32:05 -0500

    input parameters variables

--- inputvariables.sh
+++ inputvariables.sh
@@ -4,7 +4,9 @@ echo -e "Script name \e[34m $0 \e[0m"
 
 echo -e "1st argument passed to the script \e[34m $0 \e[0m is:" $1
 
-echo -e "2md argument passed to the script \e[35m $0 \e[0m is:" $1
+echo -e "2md argument passed to the script \e[35m $0 \e[0m is:" $2
+
+echo -e "3rd argument passed to the script \e[35m $0 \e[0m is:" $3
 
 echo -e "All the arguments passed to the script \e[31m $0 \e[0m are:" $@
 
