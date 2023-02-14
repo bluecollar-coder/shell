afa4e3af29860e35e072ab49a2fc5deebbe16f64
bluecollar-coder <anishkumarmalik@gmail.com>
2023-02-13 19:45:43 -0500

    code changes

--- learning-shell.sh
+++ learning-shell.sh
@@ -10,6 +10,16 @@ ls test.sh
 ls 'test.sh'
 ls "$SHELL"
 
+# '>' will redirect output to '/tmp/out' & display error on screen
 ls > /tmp/out
+# '1>' will redirect output only to '/tmp/out' file below
+# '2>' will redirect error only to '/tmp/out' file below
+ls -ld /boot /boot1 1> /tmp/out 2>/tmp/err
+# '&>' will redirect both output and error to '/tmp/out'
+ls &> /tmp/out
+
+# Sometimes, if we want to execute some command and don't want output or error anywhere then use a nullify location on system '/dev/null'
+ls &> /dev/null
+
 ls -e "\e[33m hello \e[0m"
 ls -e "\e[32m hello again\e[0m"
---
+++ print.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# echo will print some message on the screen
+
+echo abcd
+echo Hello World
+
+# to invoke editor use '-e' and then you can use escape sequences like '\n' ,'\a', '\e','\t' etc.
+echo -e "A\nB, C\tD"
