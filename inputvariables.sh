63bf7b07d0780aa51972ea5a509a131143363d1b
bluecollar-coder <anishkumarmalik@gmail.com>
2023-02-14 18:29:59 -0500

    input parameters variables

---
+++ inputvariables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Script name is:" $0
+
+echo -e "1st argument passed to the script \e[34m $0 \e[0m is:" $1
+
+echo -e "2md argument passed to the script \e[35m $0 \e[0m is:" $1
+
+echo -e "All the arguments passed to the script \e[31m $0 \e[0m are:" $@
+
+echo -e "Total number of arguments passed to the script \e[32m $0 \e[0m are:" $#
