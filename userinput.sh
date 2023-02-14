58ea1408018e35493c7232beb354d0bc2ff86f3f
bluecollar-coder <anishkumarmalik@gmail.com>
2023-02-14 18:17:00 -0500

    Taking input from user

---
+++ userinput.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo -p "Enter your name:" name
+echo -e "Thanks for entering your name: \e[32m $name \e[0m"
+echo -p "Enter to the course:" course
+echo "Welcome $name! you can now join the course $course"
